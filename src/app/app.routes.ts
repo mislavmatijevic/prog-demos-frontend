f7a8abcca9f64d1643c3faa0379d7ccf21646b2a
Mislav Matijević <mislav.mim@gmail.com>
2024-07-29 13:32:06 +0200

    Add description about my website. A bit pretencious, ain't it?

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './main/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
+];
