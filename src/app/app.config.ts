3c369ae5423fb1339b4263ee9d76a05d61a80a51
Mislav Matijević <mislav.mim@gmail.com>
2024-08-04 21:10:06 +0200

    Introduce request caching. Currently caches only public videos (only endpoint so far). Done following this article: https://medium.com/@e94kai/cache-http-response-in-angular-78ce12988b53 The solution could be expanded with more precise TTLs per request once domain and actual server are in place.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { httpCacheInterceptor } from '../http-cache.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch())],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        httpCacheInterceptor({
+          urlsToCache: ['/videos/public'],
+          globalTTL: 60000,
+        }),
+      ])
+    ),
+  ],
 };
---
+++ src/http-cache.interceptor.ts
@@ -0,0 +1,81 @@
+import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
+import { Subject, filter, finalize, of, tap } from 'rxjs';
+
+const requests = new Map<
+  string,
+  {
+    src: string;
+    data: HttpResponse<any>;
+    data$: Subject<HttpResponse<any>>;
+    params?: any;
+    ttl?: number;
+  }
+>();
+
+export const httpCacheInterceptor = (options?: {
+  urlsToCache?: string[];
+  ttls?: { [url: string]: number };
+  globalTTL?: number;
+}) => {
+  const { urlsToCache = [], ttls, globalTTL } = options ?? {};
+  const fn: HttpInterceptorFn = (req, next) => {
+    const key = `${req.method}_${
+      req.url
+    }_${req.params.toString()}_${JSON.stringify(req.body)}`;
+
+    let useCache = urlsToCache.some((x) => new RegExp(`.*${x}$`).test(req.url));
+
+    const getPreviousRequest = () => {
+      return requests.get(key);
+    };
+
+    const prevReq = getPreviousRequest();
+    const getTTL = () => {
+      return new Date().getTime() + (ttls?.[req.url] ?? globalTTL ?? 0);
+    };
+
+    if (useCache) {
+      if (prevReq) {
+        let { data, data$, ttl } = prevReq;
+
+        if (!data$.closed) {
+          return prevReq.data$;
+        }
+
+        if (data && ttl && ttl > new Date().getTime()) {
+          return of(prevReq.data);
+        }
+
+        prevReq.data$ = new Subject<any>();
+      } else {
+        requests.set(key, {
+          src: req.url,
+          data$: new Subject<HttpResponse<any>>(),
+          data: new HttpResponse<any>(),
+          params: req.body,
+          ttl: getTTL(),
+        });
+      }
+    }
+
+    return next(req).pipe(
+      filter((x) => x instanceof HttpResponse),
+      tap((x) => {
+        const data = x as HttpResponse<any>;
+        const r = getPreviousRequest();
+        if (!r) return;
+
+        r.data = data;
+        r.ttl = getTTL();
+        !r.data$.closed && r.data$.next(data);
+      }),
+      finalize(() => {
+        const r = getPreviousRequest();
+        r?.data$.complete();
+        r?.data$.unsubscribe();
+      })
+    );
+  };
+
+  return fn;
+};
