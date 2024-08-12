f9fec2d7adb571a157645a075e34a348d13952ad
Mislav Matijević <mislav.mim@gmail.com>
2024-08-12 17:13:44 +0200

    Vastly improve navigation drawer. Now, it also can be minimized/maximized.

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,18 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Button } from 'primeng/button';
 import { NavigationItem } from '../../../types/general';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, Button],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
 export class NavigationComponent {
+  isCollapsed: boolean = false;
+
   navigationHeaders: Array<NavigationItem> = [
     { name: 'Uči', link: '/demos' },
     { name: 'Vježbaj', link: '/prog' },
   ];
+
+  toggleVisibility() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
