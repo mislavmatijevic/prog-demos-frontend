5413b6d2d4dbfe6295a175360d565ba80bde4790
Mislav Matijević <mislav.mim@gmail.com>
2024-08-21 19:48:23 +0200

    Vastly rework http interceptor to also work with error responses.

--- src/http-cache.interceptor.ts
+++ src/http-cache.interceptor.ts
@@ -1,16 +1,21 @@
-import { HttpInterceptorFn, HttpResponse } from '@angular/common/http';
-import { Subject, filter, finalize, of, tap } from 'rxjs';
+import {
+  HttpErrorResponse,
+  HttpInterceptorFn,
+  HttpResponse,
+} from '@angular/common/http';
+import { Subject, of, throwError } from 'rxjs';
+import { finalize, tap } from 'rxjs/operators';
 
-const requests = new Map<
-  string,
-  {
-    src: string;
-    data: HttpResponse<any>;
-    data$: Subject<HttpResponse<any>>;
-    params?: any;
-    ttl?: number;
-  }
->();
+type CachedRequest = {
+  params?: any;
+  ttl?: number;
+  didSucceed: boolean;
+  successData: HttpResponse<any>;
+  successData$: Subject<HttpResponse<any>>;
+  errorData: HttpErrorResponse;
+};
+
+const requests = new Map<string, CachedRequest>();
 
 export const httpCacheInterceptor = (options?: {
   urlsToCache?: string[];
@@ -18,63 +23,94 @@ export const httpCacheInterceptor = (options?: {
   globalTTL?: number;
 }) => {
   const { urlsToCache = [], ttls, globalTTL } = options ?? {};
+
   const fn: HttpInterceptorFn = (req, next) => {
+    const getPreviousRequestLikeThis = () => requests.get(key);
+    const checkRequestTTL = (req: CachedRequest) => {
+      return req.ttl && req.ttl > new Date().getTime();
+    };
+
+    requests.forEach((value, cacheKey) => {
+      if (!checkRequestTTL(value)) {
+        requests.delete(cacheKey);
+      }
+    });
+
     const key = `${req.method}_${
       req.url
     }_${req.params.toString()}_${JSON.stringify(req.body)}`;
-
-    let useCache = urlsToCache.some((x) => new RegExp(`.*${x}$`).test(req.url));
-
-    const getPreviousRequest = () => {
-      return requests.get(key);
-    };
-
-    const prevReq = getPreviousRequest();
-    const getTTL = () => {
-      return new Date().getTime() + (ttls?.[req.url] ?? globalTTL ?? 0);
-    };
+    const useCache = urlsToCache.some((x) =>
+      new RegExp(`.*${x}$`).test(req.url)
+    );
+    const prevReq = getPreviousRequestLikeThis();
+    const getTTL = () =>
+      new Date().getTime() + (ttls?.[req.url] ?? globalTTL ?? 0);
 
     if (useCache) {
       if (prevReq) {
-        let { data, data$, ttl } = prevReq;
-
-        if (!data$.closed) {
-          return prevReq.data$;
+        if (prevReq.didSucceed && !prevReq.successData$.closed) {
+          return prevReq.successData$.asObservable();
         }
 
-        if (data && ttl && ttl > new Date().getTime()) {
-          return of(prevReq.data);
+        if (checkRequestTTL(prevReq)) {
+          if (prevReq.didSucceed && prevReq.successData) {
+            return of(prevReq.successData);
+          } else if (prevReq.errorData) {
+            return throwError(
+              () =>
+                new HttpResponse<HttpErrorResponse>({
+                  url: prevReq.errorData.url ?? undefined,
+                  body: prevReq.errorData.error,
+                  headers: prevReq.errorData.headers,
+                  status: prevReq.errorData.status,
+                  statusText: prevReq.errorData.statusText,
+                })
+            );
+          }
+        } else {
+          requests.delete(key);
         }
-
-        prevReq.data$ = new Subject<any>();
       } else {
         requests.set(key, {
-          src: req.url,
-          data$: new Subject<HttpResponse<any>>(),
-          data: new HttpResponse<any>(),
+          successData$: new Subject<HttpResponse<any>>(),
+          successData: new HttpResponse(),
           params: req.body,
           ttl: getTTL(),
+          didSucceed: false,
+          errorData: new HttpErrorResponse({ url: req.url }),
         });
+
+        return next(req).pipe(
+          tap({
+            next: (res) => {
+              const req = getPreviousRequestLikeThis();
+
+              if (!req) return;
+
+              if (res instanceof HttpResponse) {
+                req.didSucceed = true;
+                req.successData = res;
+                req.ttl = getTTL();
+                req.successData$.next(res);
+                req.successData$.complete();
+              } else if (res instanceof HttpErrorResponse) {
+                req.didSucceed = false;
+                req.errorData = res;
+
+                req.ttl = getTTL();
+                req.successData$.complete();
+              }
+            },
+          }),
+          finalize(() => {
+            const req = getPreviousRequestLikeThis();
+            req?.successData$.unsubscribe();
+          })
+        );
       }
     }
 
-    return next(req).pipe(
-      filter((x) => x instanceof HttpResponse),
-      tap((x) => {
-        const data = x as HttpResponse<any>;
-        const r = getPreviousRequest();
-        if (!r) return;
-
-        r.data = data;
-        r.ttl = getTTL();
-        !r.data$.closed && r.data$.next(data);
-      }),
-      finalize(() => {
-        const r = getPreviousRequest();
-        r?.data$.complete();
-        r?.data$.unsubscribe();
-      })
-    );
+    return next(req);
   };
 
   return fn;
