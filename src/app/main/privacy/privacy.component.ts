31813adcfcdc2d57ea45e455148a84287a213d39
Mislav Matijević <mislav.mim@gmail.com>
2024-10-05 20:29:28 +0200

    Edit privacy policy. Boring legal stuff now looks nicer. And sucks at scrolling rip.

--- src/app/main/privacy/privacy.component.ts
+++ src/app/main/privacy/privacy.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './privacy.component.html',
-  styleUrl: './privacy.component.scss'
+  styleUrl: './privacy.component.scss',
 })
 export class PrivacyComponent {
-
+  scrollToElementWithId(elementId: string) {
+    document.getElementById(elementId)?.scrollIntoView(false);
+  }
 }
