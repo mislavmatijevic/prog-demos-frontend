d082c52585efeef5b3090c5a00dd308b47e37ba3
Mislav Matijević <mislav.mim@gmail.com>
2024-11-14 17:42:15 +0100

    Add space in `include` directive of CPP starter code.

--- src/helpers/editor-helpers.ts
+++ src/helpers/editor-helpers.ts
@@ -1,2 +1,2 @@
 export const standardCppStarterCode =
-  '#include<iostream>\n\nusing namespace std;\n\nint main() {\n\n    return 0;\n}\n';
+  '#include <iostream>\n\nusing namespace std;\n\nint main() {\n\n    return 0;\n}\n';
