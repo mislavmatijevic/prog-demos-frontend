53a00b7ccf373c1b16d8bdfaa08af13abedb3dad
Mislav Matijević <mislav.mim@gmail.com>
2025-10-27 20:18:08 +0100

    Implement design for report-error form. Set default input style across entire page. I untangled some of my own spaghetti code now so inputs actually use consistent style across the page...

--- src/app/components/report-error/report-error.ts
+++ src/app/components/report-error/report-error.ts
@@ -1,32 +1,58 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Event, NavigationStart, Router } from '@angular/router';
-import { Button } from 'primeng/button';
-import { TooltipModule } from 'primeng/tooltip';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DialogModule } from 'primeng/dialog';
+import { DividerModule } from 'primeng/divider';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { AuthService } from '../../services/auth.service';
 
 const exemptViews = ['/', '/privacy'];
 
 @Component({
   selector: 'app-report-error',
   standalone: true,
-  imports: [CommonModule, Button, TooltipModule],
+  imports: [
+    CommonModule,
+    DividerModule,
+    ButtonModule,
+    InputTextModule,
+    FloatLabelModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ProgressSpinnerModule,
+    DialogModule,
+    CheckboxModule,
+  ],
   templateUrl: './report-error.html',
   styleUrl: './report-error.scss',
 })
 export class ReportError implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
-  protected isHidden: boolean = false;
+  isHidden: boolean = false;
+  dialogOpened: boolean = true;
+  reportName = new FormControl('');
+  reportBody = new FormControl('');
+  isLoggedIn = this.authService.isLoggedIn;
 
   ngOnInit(): void {
     this.router.events.subscribe((event: Event) => {
       if (event instanceof NavigationStart) {
+        this.reportName.setValue(`Greška na '${event.url.substring(1)}' ruti`);
+        this.reportBody.setValue(
+          `Prijavljujem grešku koja se pojavljuje na stranici. Kada otvoriš www.progdemos.com${event.url}, mislim da bi umjesto da se pojavi A trebalo biti B (već štogod)`
+        );
         this.isHidden = exemptViews.includes(event.url);
       }
     });
   }
 
   openReportBugDialog() {
-    throw new Error('Method not implemented.');
+    this.dialogOpened = true;
   }
 }
